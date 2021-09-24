d4f3fbd924392adbc536dcacd417541aadb62edb
Arece <steeph.rv@gmail.com>
2021-09-24 14:00:09 -0500

    subiendo por primera vez el tp1

---
+++ TP_[1]/src/funciones_Calculadora.c
@@ -0,0 +1,126 @@
+/*
+ * funciones_Calculadora.c
+ *
+ *  Created on: 21 set. 2021
+ *      Author: Steeph Rios Valdivia
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "funciones_Calculadora.h"
+
+
+
+ int menu(int flagOp1,int flagOp2,int num1, int num2)
+{
+	int opcion;
+
+	printf("Menu de opciones\n\n");
+	printf("1. Primer operador: ");
+	if(flagOp1==1){
+		printf("X = %d\n",num1);
+	}
+	else{
+		printf("\n");
+	}
+
+	printf("2. Segundo operador: ");
+	if(flagOp2==1){
+
+		printf("Y = %d\n",num2);
+	}
+	else{
+		printf("\n");
+	}
+
+	printf("3. Calcular todas las operaciones\n");
+	printf("   * Suma (A+B)\n");
+	printf("   * Resta (A-B)\n");
+	printf("   * Multiplicacion (A*B)\n");
+	printf("   * Division (A/B)\n");
+	printf("   * Factorial (A!) y (B!)\n");
+	printf("4. Mostrar resultados\n");
+	printf("5. SALIR: ");
+	printf("\nIndique opcion: ");
+	fflush(stdin);
+	scanf("%d", &opcion);
+
+	return opcion;
+}
+
+int ingresarEntero( char mensaje[])
+{
+	int numeroIngresado;
+
+	printf("%s", mensaje);
+	scanf("%d", &numeroIngresado);
+
+	return numeroIngresado;
+}
+
+int sumar (int num1, int num2)
+{
+	int resultadoSum;
+
+	resultadoSum= num1+num2;
+
+	return resultadoSum;
+
+}
+
+int restar(int num1, int num2)
+{
+	int resultadoRest;
+
+	resultadoRest=num1-num2;
+
+	return resultadoRest;
+}
+
+int multiplicar(int num1, int num2)
+{
+	int resultadoMulti;
+
+	resultadoMulti=num1*num2;
+
+	return resultadoMulti;
+}
+
+int dividir( int num1, int num2, float* pResultado)
+{
+	float resultadoDivi;
+	int retorno=-1;
+
+	if(pResultado!= NULL && num2 != 0)
+	{
+		resultadoDivi= (float) num1/num2;
+		*pResultado= resultadoDivi;
+		retorno=0;
+	}
+
+	return retorno;
+}
+
+long long int factorial(int numeroIngresado,int* pResultado)
+{
+
+
+	int resultadoFact=0;
+	int retorno=0;
+	if(pResultado != NULL && numeroIngresado>=0)
+	{
+		resultadoFact=1;
+		for(int i =2 ; i<= numeroIngresado ; i++)
+		{
+			resultadoFact=resultadoFact*i;
+		}
+		retorno=1;
+	}
+	*pResultado=resultadoFact;
+
+	return retorno;
+}
+
+
+
